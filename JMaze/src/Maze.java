a78bb5708af6b67c9fc3310937fbbdc6478c7fd9
ryanliddle <ryan.liddle@hotmail.com>
2014-05-14 22:00:46 +1000

    Testing

--- JMaze/src/Maze.java
+++ JMaze/src/Maze.java
@@ -2,7 +2,9 @@
 public class Maze {
 	public static void main (String args[])
 	{
+		//GITHUB WORKS WITH ECLIPSE NOW
+		//FINALLY
 		System.out.println("This is out whole maze");
-		//Comment
+		
 	}
 }
